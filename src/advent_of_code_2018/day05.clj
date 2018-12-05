60c6f970d281dd58a3d61f81f5ff535e23f9d13e
genmeblog <generateme.blog@gmail.com>
2018-12-05 15:51:27 +0100

    slack fix - speed!

--- src/advent_of_code_2018/day05.clj
+++ src/advent_of_code_2018/day05.clj
@@ -2,35 +2,29 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as s]))
 
+(set! *unchecked-math* :warn-on-boxed)
+
 (def polymer (->> (slurp (io/resource "day05.txt"))
                   (s/trim)
                   (map int)
                   (delay)))
 
-(defn react-step
-  [[^long prev-unit coll] ^long curr-unit]
-  (if prev-unit
-    (if (== 32 (Math/abs (- prev-unit curr-unit)))
-      [nil coll]
-      [curr-unit (conj coll prev-unit)])
-    [curr-unit coll]))
-
 (defn react
-  [[curr-cnt coll]]
-  (let [[o re] (reduce react-step [nil []] coll)
-        reacted (if o (conj re o) re)
-        cnt (count reacted)]
-    (if (= curr-cnt cnt)
-      [cnt reacted]
-      (recur [cnt reacted]))))
+  "Approach built after discussion on slack."
+  ([poly] (react nil poly))
+  ([back poly]
+   (if-let [^int c (first poly)]
+     (if (and (seq back) (== 32 (Math/abs (- ^int (first back) c))))
+       (recur (rest back) (rest poly))
+       (recur (cons c back) (rest poly)))
+     back)))
 
 (def removed-unit-reactions
-  (delay (pmap (fn [^long lower] (let [capital (- lower 32)
-                                       npolymer (filter #(not (or (== ^int % lower)
-                                                                  (== ^int % capital))) @polymer)]
-                                   (first (react [-1 npolymer]))))
-               (range (int \a) (inc (int \z))))))
+  (delay (pmap (fn [^long upper ^long lower]
+                 (let [npolymer (remove #(or (== ^int % lower)
+                                             (== ^int % upper)) @polymer)]
+                   (count (react npolymer)))) (range 97 123) (range 65 91))))
 
-(time {:reaction-units (first (react [-1 @polymer]))
+(time {:reaction-units (count (react @polymer))
        :shortest (apply min @removed-unit-reactions)})
 ;; => {:reaction-units 11540, :shortest 6918}
